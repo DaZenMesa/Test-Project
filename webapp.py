819b52e86cdc987050b5dae160d82b979df18338
Mesa Diehl <42652916+DaZenMesa@users.noreply.github.com>
2018-10-09 10:30:36 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,5 +13,29 @@ def render_floppy():
 def render_heatsink():
     return render_template('heatsink.html')
 
+@app.route("/harddrive")
+def render_hardrive():
+    return render_template('harddrive.html')
+
+@app.route("/motherboard")
+def render_motherboard():
+    return render_template('motherboard.html')
+
+@app.route("/opticaldrive")
+def render_opticaldrive():
+    return render_template('opticaldrive.html')
+
+@app.route("/powersupply")
+def render_powersupply():
+    return render_template('powersupply.html')
+
+@app.route("/ram")
+def render_ram():
+    return render_template('ram.html')
+
+@app.route("/systemfan")
+def render_systemfan():
+    return render_template('systemfan.html')
+
 if __name__=="__main__":
     app.run(debug=False, port=54321)
